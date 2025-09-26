00ed8628ada4b171a16999ac3d224c8c1fc3b2ad
Konstantin1521 <gorbachevskiy.konstantin@gmail.com>
2025-09-26 23:45:14 +0300

    Добавил файл конфигурации

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	ServerPort  string `mapstructure:"SERVER_PORT"`
+	DatabaseURL string `mapstructure:"DATABASE_URL"`
+}
+
+func LoadConfig() (Config, error) {
+	var cfg Config
+
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath("/")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Printf("Config file not found, using ENV only: %v", err)
+	}
+
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return cfg, err
+	}
+
+	return cfg, nil
+}
