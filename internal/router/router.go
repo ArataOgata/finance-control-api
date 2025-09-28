c2e7579b8b54098ed9f7307de2fe6c4bbc60d884
Konstantin1521 <gorbachevskiy.konstantin@gmail.com>
2025-09-28 22:24:15 +0300

    подключение всех слоев вместе

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,15 +1,26 @@
 package router
 
 import (
-	"go-api/internal/handlers"
-
 	"github.com/go-chi/chi/v5"
+
+	"go-api/internal/db"
+	"go-api/internal/handlers"
+	"go-api/internal/repository"
+	"go-api/internal/service"
 )
 
 func NewRouter() *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Get("/ping", handlers.PingHandler)
+
+	userRepo := repository.NewUserRepository(db.DB)
+	userService := service.NewUserService(userRepo)
+	userHandler := handlers.NewUserHandler(userService)
+
+	// public
+	r.Post("/register", userHandler.Register)
+	r.Get("/user", userHandler.GetUser)
 
 	return r
 }
