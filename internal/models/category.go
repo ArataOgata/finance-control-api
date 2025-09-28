0032c2bfda02e86270feae33a9846a23a21ccdd7
Konstantin1521 <gorbachevskiy.konstantin@gmail.com>
2025-09-29 00:09:37 +0300

    обновление модели

--- internal/models/category.go
+++ internal/models/category.go
@@ -5,7 +5,7 @@ import "time"
 // Category — модель для таблицы categories
 type Category struct {
 	CategoryID  uint      `gorm:"primaryKey;autoIncrement" json:"category_id"`
-	Title       string    `gorm:"size:60;unique;not null" json:"title"`
+	Title       string    `gorm:"size:60;index:idx_user_title;not null" json:"title"`
 	Description string    `gorm:"size:300" json:"description"`
 	Total       int       `gorm:"default:0" json:"total"`
 	CreatedAt   time.Time `gorm:"autoCreateTime" json:"created_at"`
