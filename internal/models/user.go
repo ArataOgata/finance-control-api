ddab508b8a26ead04f2f5cb398121e7b448c2fe0
Konstantin1521 <gorbachevskiy.konstantin@gmail.com>
2025-09-28 22:22:45 +0300

    обновление модели пользователей

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,5 +3,6 @@ package models
 type User struct {
 	UserID   uint   `gorm:"primaryKey;autoIncrement" json:"user_id"`
 	Username string `gorm:"size:25;unique;not null" json:"username"`
+	Tg_id    int    `gorm:"unique;not null"`
 	Balance  int    `gorm:"default:0" json:"balance"`
 }
